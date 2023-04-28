8f6ae1738128a12322dd61d6f9204d1108b9706f
elnur-elnur <tahirzadee@gmail.com>
2023-04-29 01:18:53 +0400

    created login / register module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async register(dto: CreateUserDto) {
+    try {
+      const userData = this.usersService.create(dto);
+
+      return userData;
+    } catch (error) {
+      console.log(error);
+
+      throw new ForbiddenException('Qeydiyyat zamanı səhf');
+    }
+  }
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   fullName: string;
+
+  @ApiProperty()
   password: string;
 }
