52b04bfbf3bed8b4cdcf9d8e7223b561f00a273c
elnur-elnur <tahirzadee@gmail.com>
2023-04-28 15:24:22 +0400

    created swagger / installed db / connected to ElephantSQL / created User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'manny.db.elephantsql.com',
+      port: 5432,
+      username: 'xcavvvtu',
+      password: '2LdqN_Hp5WQ_7AVUo8pgKmQBh2bspJOG',
+      database: 'xcavvvtu',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
